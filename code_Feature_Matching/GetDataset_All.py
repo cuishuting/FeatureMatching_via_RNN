29ec498d7a4efacce2b600ff287766ab4d681422
cuishuting <48686364+cuishuting@users.noreply.github.com>
2022-12-26 13:42:47 -0600

    upload Get_Feature_Embeddings_Test.sh

--- code_Feature_Matching/GetDataset_All.py
+++ code_Feature_Matching/GetDataset_All.py
@@ -10,7 +10,7 @@ import os
 
 
 class OrgMIMICIIIDataset(Dataset):
-    def __init__(self, path):
+    def __init__(self, path, features_belongs):
         self.all_data = np.load(path, allow_pickle=True)
         self.static_features_list = []
         self.temporal_features_list = []
@@ -18,20 +18,22 @@ class OrgMIMICIIIDataset(Dataset):
         self.targets_list = []
         self.num_data = len(self.all_data['y_mor'])
         # load static variables
-        if os.path.exists("./StaticFeatures_08_17.pt"):
-            self.static_features_list = torch.load("./StaticFeatures_08_17.pt")
+        if os.path.exists("/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/StaticFeatures_" + features_belongs + ".pt"):
+            self.static_features_list = torch.load("/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/StaticFeatures_" + features_belongs + ".pt")
             print("Successfully load static variables!\n")
         else:
             for s in range(self.num_data):
                 if (s+1) % 3000 == 0:
                     print("currently transform (static feature): ", s + 1)
-                self.static_features_list.append(torch.tensor(self.all_data["adm_features_all"][s]).float())
+                cur_static_feature = torch.tensor(self.all_data["adm_features_all"][s])
+                cur_static_feature[cur_static_feature != cur_static_feature] = 0
+                self.static_features_list.append(cur_static_feature.float())
 
-            torch.save(self.static_features_list, "StaticFeatures_08_17.pt")
+            torch.save(self.static_features_list, "/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/StaticFeatures_" + features_belongs + ".pt")
 
         # load temporal features
-        if os.path.exists("./TemporalFeatures_08_17.pt"):
-            self.temporal_features_list = torch.load("./TemporalFeatures_08_17.pt")
+        if os.path.exists("/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/TemporalFeatures_" + features_belongs + ".pt"):
+            self.temporal_features_list = torch.load("/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/TemporalFeatures_" + features_belongs + ".pt")
             print("Successfully load temporal features!\n")
         else:
             for t in range(self.num_data):
@@ -42,11 +44,11 @@ class OrgMIMICIIIDataset(Dataset):
                 cur_temporal_feature[cur_temporal_feature != cur_temporal_feature] = 0
                 self.temporal_features_list.append(cur_temporal_feature.float())
 
-            torch.save(self.temporal_features_list, "TemporalFeatures_08_17.pt")
+            torch.save(self.temporal_features_list, "/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/TemporalFeatures_" + features_belongs + ".pt")
 
         # load mask
-        if os.path.exists("./TemporalMasks_08_17.pt"):
-            self.temporal_masks_list = torch.load("./TemporalMasks_08_17.pt")
+        if os.path.exists("/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/TemporalMasks_" + features_belongs + ".pt"):
+            self.temporal_masks_list = torch.load("/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/TemporalMasks_" + features_belongs + ".pt")
             print("Successfully load mask!\n")
         else:
             for m in range(self.num_data):
@@ -54,19 +56,19 @@ class OrgMIMICIIIDataset(Dataset):
                     print("currently transform (mask): ", m + 1)
                 self.temporal_masks_list.append(torch.tensor(self.all_data['ep_tdata_masking'][m]))
 
-            torch.save(self.temporal_masks_list, "TemporalMasks_08_17.pt")
+            torch.save(self.temporal_masks_list, "/storage1/christopherking/Active/mimic3/c.shuting/MIMICIII_DeepRecurrent_Models/Train_Tensors/TemporalMasks_" + features_belongs + ".pt")
 
         # load targets: a) data['y_icd9'][6] (t7) b) data['y_mor'] (mortality label)
-        if os.path.exists("./Targets_08_17.pt"):
+        if os.path.exists("/Targets_08_17.pt"):
             self.targets_list = torch.load("./Targets_08_17.pt")
             print("Successfully load targets!\n")
         else:
             for i in range(self.num_data):
                 if (i + 1) % 3000 == 0:
                     print("currently transform targets: ", i + 1)
-                cur_t7 = torch.tensor([self.all_data["y_icd9"][i][6]])
+                cur_icd9_labels = torch.tensor(self.all_data["y_icd9"][i])
                 cur_y_mor = torch.tensor(self.all_data["y_mor"][i])
-                cur_target = torch.cat((cur_t7, cur_y_mor), 0)
+                cur_target = torch.cat((cur_icd9_labels, cur_y_mor), 0)
                 self.targets_list.append(cur_target)
 
             torch.save(self.targets_list, "Targets_08_17.pt")
@@ -76,8 +78,8 @@ class OrgMIMICIIIDataset(Dataset):
 
     def __getitem__(self, idx):
         sample_static_feature = self.static_features_list[idx]
-        sample_temporal_feature = self.temporal_features_list[idx]
-        sample_temporal_mask = self.temporal_masks_list[idx]
+        sample_temporal_feature = self.temporal_features_list[idx]  # [24, 12]
+        sample_temporal_mask = self.temporal_masks_list[idx]  # [24, 12]
         sample_target = self.targets_list[idx]
         return sample_static_feature, sample_temporal_feature, sample_temporal_mask, sample_target
 
---
+++ code_Feature_Matching/Get_Feature_Embeddings_Test.sh
@@ -0,0 +1,7 @@
+#BSUB -o Get_Feature_embeddings_model_2_Test.%J
+#BSUB -N
+#BSUB -J Get_Feature_embeddings_model_2_Test
+#BSUB -gpu "num=4:mode=exclusive_process:gmodel=NVIDIAGeForceGTX1080Ti"
+#BSUB -q normal
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/research/c.shuting/anaconda3/lib
+python main_get_emb.py
